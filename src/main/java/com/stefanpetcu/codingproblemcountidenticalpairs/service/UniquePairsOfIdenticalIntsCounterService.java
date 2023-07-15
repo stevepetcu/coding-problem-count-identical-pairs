332d53d0473117ce5e99feb41a93530fabc2d55a
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-07-15 14:52:21 +0100

    ✅ Test logic for the maximum number of pairs we should return.

--- src/main/java/com/stefanpetcu/codingproblemcountidenticalpairs/service/UniquePairsOfIdenticalIntsCounterService.java
+++ src/main/java/com/stefanpetcu/codingproblemcountidenticalpairs/service/UniquePairsOfIdenticalIntsCounterService.java
@@ -4,28 +4,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class UniquePairsOfIdenticalIntsCounterService {
+    private static final int MAX_SUPPORTED_NUMBER_OF_PAIRS_COUNT = 1_000_000_000;
+    // Any list of identical ints that's got more than IDENTICAL_INTS_LIST_UPPER_SIZE_THRESHOLD elements
+    // will have over MAX_SUPPORTED_NUMBER_OF_PAIRS_COUNT pairs. We can return early without parsing the
+    // remaining inputs or even calculating the number of pairs. Cheats!
+    private static final int IDENTICAL_INTS_LIST_UPPER_SIZE_THRESHOLD = 44721;
     public int count(int[] arr) {
         Arrays.sort(arr);
 
-        var pairs = new ArrayList<Integer>();
+        var identicalIntsList = new ArrayList<Integer>();
         int pairCount = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            if (pairs.size() == 0) {
-                pairs.add(arr[i]);
+            if (identicalIntsList.size() == 0) {
+                identicalIntsList.add(arr[i]);
             } else {
-                if (arr[i] == pairs.get(0)) {
-                    pairs.add(arr[i]);
+                if (arr[i] == identicalIntsList.get(0)) {
+                    identicalIntsList.add(arr[i]);
                 }
-                if (arr[i] != pairs.get(0) || i == arr.length - 1) {
-                    pairCount += calculateCombinations(pairs);
+                if (identicalIntsList.size() > IDENTICAL_INTS_LIST_UPPER_SIZE_THRESHOLD) {
+                    return MAX_SUPPORTED_NUMBER_OF_PAIRS_COUNT;
+                }
+                if (arr[i] != identicalIntsList.get(0) || i == arr.length - 1) {
+                    pairCount += calculateCombinations(identicalIntsList);
 
-                    if (pairCount >= 1_000_000_000) {
-                        return 1_000_000_000;
+                    if (pairCount >= MAX_SUPPORTED_NUMBER_OF_PAIRS_COUNT) {
+                        return MAX_SUPPORTED_NUMBER_OF_PAIRS_COUNT;
                     }
 
-                    pairs.clear();
-                    pairs.add(arr[i]);
+                    identicalIntsList.clear();
+                    identicalIntsList.add(arr[i]);
                 }
             }
         }
--- src/test/java/com/stefanpetcu/codingproblemcountidenticalpairs/service/UniquePairsOfIdenticalIntsCounterServiceTest.java
+++ src/test/java/com/stefanpetcu/codingproblemcountidenticalpairs/service/UniquePairsOfIdenticalIntsCounterServiceTest.java
@@ -9,6 +9,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UniquePairsOfIdenticalIntsCounterServiceTest {
+    private static final int REALLY_BIG_ARRAY_SIZE = 10000000; // Any order of mag larger crashes the program.
     private final UniquePairsOfIdenticalIntsCounterService subject = new UniquePairsOfIdenticalIntsCounterService();
 
     private static Stream<Arguments> intArraysProvider() {
@@ -17,7 +18,8 @@ class UniquePairsOfIdenticalIntsCounterServiceTest {
                 Arguments.of(new int[]{0}, 0),
                 Arguments.of(new int[]{1, 1}, 1),
                 Arguments.of(new int[]{3, 5, 6, 3, 3, 5}, 4),
-                Arguments.of(new int[]{-1, 3, 5, -10, 6, 3, 3, 5, -1, 5, 3, -10, 3, -1, 5, -10}, 22)
+                Arguments.of(new int[]{-1, 3, 5, -10, 6, 3, 3, 5, -1, 5, 3, -10, 3, -1, 5, -10}, 22),
+                Arguments.of(new int[REALLY_BIG_ARRAY_SIZE], 1_000_000_000) // An array filled with 0s.
         );
     }
 
